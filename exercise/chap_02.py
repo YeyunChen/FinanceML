7166ec16cfb8b2cbfa1b07099aca746d1936d959
yeyunchen <yeyunchen@outlook.com>
2023-05-09 14:33:58 +0100

    finished solution for 2.1 (a)

--- exercise/chap_02.py
+++ exercise/chap_02.py
@@ -1,14 +1,15 @@
-from data import data_generator as dataGen
+from data import data_generator as data_gen
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def load_data():
-    data = dataGen.create_tick_data(n_samples=100000)
+    data = data_gen.create_tick_data(n_samples=1000000)
     return data
 
 
-def form_tick_bar(data: pd.DataFrame, bar_size: int = 100):
+def form_tick_bar(data: pd.DataFrame, bar_size: int = 10):
     data['tick_count'] = range(len(data))
     data['tick_count_resi'] = data['tick_count'] % bar_size
     tick_bar = data.loc[data['tick_count_resi'] == 0, ['price']]
@@ -32,12 +33,28 @@ def form_dollar_bar(data: pd.DataFrame, bar_size: int = 10000000):
     return dollar_bar
 
 
+def count_bars():
+    # solution for exercise 2.1
+
+    data = load_data()
+    tick_bar = form_tick_bar(data)
+    volume_bar = form_volume_bar(data)
+    dollar_bar = form_dollar_bar(data)
+
+    tick_bar_count = tick_bar.resample('1min').count()
+    volume_bar_count = volume_bar.resample('1min').count()
+    dollar_bar_count = dollar_bar.resample('1min').count()
+
+    count_df = pd.concat([tick_bar_count, volume_bar_count, dollar_bar_count], axis=1)
+    count_df.columns = ['tick', 'volume', 'dollar']
+
+    count_df.plot(kind='line', figsize=[25, 5])
+    plt.show()
+
+
 def roll_futures():
     return 1
 
 
 if __name__ == '__main__':
-    data = load_data()
-    tick_bar = form_tick_bar(data)
-    volume_bar = form_volume_bar(data)
-    dollar_bar = form_dollar_bar(data)
+    count_bars()
